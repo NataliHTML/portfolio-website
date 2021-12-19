0c45f76347fa9fd0435ab09a3f0a7a00b44a56f2
NataliHTML <nataly07081992@gmail.com>
2021-12-19 18:06:35 +0300

    footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,26 @@
-$(function () {
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
 
+menu.onclick = () => {
+	menu.classList.toggle('fa-times');
+	header.classList.toggle('active');
+}
 
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () => {
+	themeToggler.classList.toggle('fa-sun');
+	if (themeToggler.classList.contains('fa-sun')) {
+		document.body.classList.add('active');
+	} else {
+		document.body.classList.remove('active');
+	}
+}
+
+$('[data-modal]').on('click', function (event) {
+	event.preventDefault();
+	$('#modal-resume').addClass('modal-show');
+});
+$('#modal-resume').on('click', function () {
+	$('#modal-resume').removeClass('modal-show');
 });
